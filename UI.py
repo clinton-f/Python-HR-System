a2b07d2c241445b43208de89511171b6ef9dab2b
Clinton F <legacy_7005@yahoo.com>
2022-04-11 12:49:04 -0400

    alert labels we can user

--- UI.py
+++ UI.py
@@ -3,15 +3,21 @@ import streamlit as st
 
 st.title("HR Payroll Employee Management Application")
 
-#   For the Menu Bar
+# For the Menu Bar
 st.sidebar.markdown(""" # Menu Bar # """)
 
 
-
-
 st.sidebar.markdown("""
-    
     """)
     
+# Success Alert
+st.success("Input was a success!")
 
+# Error Alert - red
+st.error("Some error!")
 
+# Warning Alert
+st.warning("Text warning!")
+
+# Info Alert
+st.info("Some info!")
