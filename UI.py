e99c294e70227a2c233e1d8e2eb26fd4067be2f3
Clinton F <legacy_7005@yahoo.com>
2022-04-11 10:49:50 -0400

    Side bar

---
+++ UI.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+st.title("HR Payroll Employee Management Application")
+
+#   For the Menu Bar
+st.sidebar.markdown(""" # Menu Bar # """)
+
+
+
+
+st.sidebar.markdown("""
+    
+    """)
+    
+
+
