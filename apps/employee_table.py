2eab3769781dd51e2c4791913b59311326c2436a
Clinton F <legacy_7005@yahoo.com>
2022-04-19 19:01:37 -0400

    UI inputs, buttons and data display done for Empl, Mgr, Exec.

--- apps/employee_table.py
+++ apps/employee_table.py
@@ -1,12 +1,42 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+
 
 def app():
-    st.title("Employee data goes under here.")
+    #st.title("Employee data goes under here.")
 
     # Info Alert
-    st.info("Some info!")
+    #st.info("Some info!")
 
     # Warning Alert
-    st.warning("Text warning!")
+    #st.warning("Text warning!")
 
-    st.write("This is the employee management page where you can edit empoyees information.")
+    
+    st.subheader("Employee Table:")
+
+    
+    with st.form(key="form1"):
+
+        #st.write(pd.DataFrame({
+        #'ID': [1, 2, 3, 4],
+        #'Employee': [10, 20, 30, 40],
+        #'Salary': [10, 20, 30, 40]}))
+
+        data = pd.DataFrame({
+        'ID': [1, 2, 3, 4],
+        'Employee Name': ['Sam', 'Shawn', 'Brook', 'Lucy'],
+        'Salary': [1021234, 222343, 32222, 42123]})
+        
+        st.table(data)
+
+        firstname, emplSal = st.beta_columns([2,2])
+        firstname.text_input("Name:") 
+        emplSal.text_input("Salary:")
+
+        submit_empl = st.form_submit_button("Submit")
+
+
+        if submit_empl:
+            st.success("Employee data was submitted!")
--- apps/executive.py
+++ apps/executive.py
@@ -1,13 +1,45 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 def app():
-    st.title("Executive")
+    #st.title("Employee data goes under here.")
 
-    # Error Alert - red
-    st.error("Some error!")
+    # Info Alert
+    #st.info("Some info!")
 
-    # Success Alert
-    st.success("Input was a success!")
+    # Warning Alert
+    #st.warning("Text warning!")
+
+    
+    st.subheader("Executive Table:")
+
+    
+    with st.form(key="form3"):
+
+        #st.write(pd.DataFrame({
+        #'ID': [1, 2, 3, 4],
+        #'Employee': [10, 20, 30, 40],
+        #'Salary': [10, 20, 30, 40]}))
+
+        data = pd.DataFrame({
+        'ID': [1, 2, 3, 4],
+        'Employee Name': ['Sam', 'Shawn', 'Brook', 'Lucy'],
+        'Salary': [1021234, 222343, 32222, 42123],
+        'Deparment': ['CSC', 'CSC', 'CSC', 'CSC'],
+        'Bonus': ['5000', '4500', '1000', '7300']})
+        
+        st.table(data)
+
+        firstname, emplSal, dept, bonus = st.beta_columns([2,2,2,2])
+        firstname.text_input("Name:") 
+        emplSal.text_input("Salary:")
+        dept.text_input("Department:")
+        bonus.text_input("Bonus:")
+        
+        submit_exec = st.form_submit_button("Submit")
+
+        if submit_exec:
+            st.success("Executive data was submitted!")
 
 
-    st.write("This is the Executive page.")
