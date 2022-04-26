6c82ae237b696e11c40057b19391b57ddfd54ff4
jtfreedman <babzibles@gmail.com>
2022-04-26 16:26:05 -0400

    adds comments

--- apps/employee_table.py
+++ apps/employee_table.py
@@ -6,20 +6,15 @@ from objects.Employee import Employee
 from objects.DataBag import DataBag
 
 def app():
-    #st.title("Employee data goes under here.")
 
-    # Info Alert
-    #st.info("Some info!")
-
-    # Warning Alert
-    #st.warning("Text warning!")
-
+    ### initialize arrays for input into Pandas DataFrame
     id = []
     names = []
     dept = []
     salary = []
     amt = 0
 
+    ### read Employees from DataBag stored in session state key
     for i in st.session_state.key:
         if i.getType() == "Employee":
             id.append(i.getID())
@@ -28,14 +23,14 @@ def app():
             salary.append(i.getSalary())
             amt += 1
 
+    ### add values to Pandas DataFrame
     data = pd.DataFrame({
     'ID': id,
     'Employee Name': names,
     'Department': dept,
     'Salary': salary})
     
-    #st.table(data)
-    
+    ### create columns and buttons for layout of UI
     empID, empName = st.columns([2,2])
     inID = empID.text_input("ID:")
     inName = empName.text_input("Name:") 
@@ -55,32 +50,40 @@ def app():
     with delete_empl:
         delete_empl = st.button("Delete by ID")
 
+    ### load and initialize Streamlit dataframe with Pandas DataFrame
     frame = st.dataframe(data=data, width=577, height=500)
 
     if add_empl:
+        ### add Employee to DataBag in session state
         st.session_state.key.add(Employee(len(st.session_state.key) + 1, inName, inDept, int(inSal)))
 
+        ### create second Pandas DataFrame to add new elements to table
         data2 = pd.DataFrame({
             'ID': [len(st.session_state.key)],
             'Employee Name': [inName],
             'Department': [inDept],
             'Salary': [int(inSal)]})
         
+        ### concat the two DataFrames
         data = pd.concat([data, data2], ignore_index=True)
 
         st.success("Employee Added")
 
+        ### clear the Streamlit dataframe and add the new data
         frame.empty()
         frame = st.dataframe(data=data, width=577, height=500)
 
     if delete_empl:
+        ### find Employee in DataBag where ID is matching
         for i in st.session_state.key:
             if i.getID() == int(inID):
 
+                ### removed Employee from the DataBag
                 st.session_state.key.remove(i)
 
                 st.success("Employee Deleted")
 
+                ### create new Pandas DataFrame and add values in
                 id = []
                 names = []
                 dept = []
@@ -99,9 +102,12 @@ def app():
                     'Department': dept,
                     'Salary': salary})
 
+                ### clear Streamlit dataframe and replace with new data
                 frame.empty()
                 frame = st.dataframe(data=data, width=577, height=500)
-
+                
+                ### indicate that the Employee was found
                 found = True
-            
+
+        ### alert the user if Employee ID cannot be found    
         if not found: st.warning("Text warning!")
