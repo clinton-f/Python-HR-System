a4dae1b5a82b7ba299deb2a4efda14cc5c084d4e
jtfreedman <babzibles@gmail.com>
2022-04-27 17:39:16 -0400

    comments and cleanup

--- app.py
+++ app.py
@@ -1,37 +1,33 @@
 import streamlit as st
 
 # Custom imports #
-
 from multipage import MultiPage
 from apps import home, employee_table, manager, executive, payroll
 from objects.DataBag import DataBag
 from objects.sourceData import loadData
 
 
+### Create header
 st.header("HR Payroll Employee Management Application")
 
-# For the Menu Bar
-
-# Adds DataBag for storage to session state
+### Adds DataBag for storage to session state
 test = DataBag()
 test = loadData()
 
 if 'key' not in st.session_state:
     st.session_state.key = test
-
-# This is the multipage section, so all pages here.
  
 
-# Create an instance of the app 
+### Create an instance of the app 
 app = MultiPage()
 
 
-# Add all your applications (pages) here
+### Add all your applications (pages) here
 app.add_page("Home", home.app)
 app.add_page("Employee", employee_table.app)
 app.add_page("Manager", manager.app)
 app.add_page("Executive", executive.app)
 app.add_page("Payroll", payroll.app)
 
-# The main app
+### Run app
 app.run()
